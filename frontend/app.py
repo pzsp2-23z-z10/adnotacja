3da34e211c46ea80e7a89a8d16f25a71b23dd79f
Dominik Sidorczuk <domik141202@gmail.com>
2023-12-07 15:59:31 +0100

    PZSP2-6 add 2 buttons and 1 method for load file

--- frontend/app.py
+++ frontend/app.py
@@ -1,14 +1,33 @@
-from flask import Flask, render_template
-from configure import PORT, HOST
+from flask import Flask, render_template, redirect
+from flask import request as rq, session, url_for
+from configure import PORT, HOST, BACKEND_PORT
 import requests
+from uuid import uuid4
+from datetime import timedelta
 
 
 app = Flask(__name__)
+app.secret_key = 'Bruhuhuhuhu64'
+app.permanent_session_lifetime = timedelta(seconds=60)
 
 
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+
+@app.route('/analysis/add', methods=['GET', 'POST'])
+def add():
+    if rq.method == 'GET':
+        return render_template('upload.html')
+    elif rq.method == 'POST':
+        file = rq.files['file']
+        return render_template('uploaded.html')
+
+
+@app.route('/analysis/status', methods=['GET'])
+def status():
+    return render_template('dashboard.html')
 
 
 if __name__ == "__main__":
