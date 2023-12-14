0e41ed7cb1c631f3b8bcc1ce034c8edda3ae39e1
Fedir T <tsupinfedor@gmail.com>
2023-12-14 13:34:18 +0100

    Removed database folder

--- database/src/main.js
+++
@@ -1,8 +0,0 @@
-let mongoose = require('mongoose');
-const { addGenotype, addCalculationProgress } = require("./database_operations");
-const server = '127.0.0.1:27017';
-const database = 'adnotacje';
-
-mongoose.connect(`mongodb://${server}/${database}`)
-addGenotype("chr10", 1.04e+08, "A", "C", [{"atrybut1": 10}, {"atrybut2": 20}]);
-addCalculationProgress([false,false,false])
--- database/src/models/calculation.js
+++
@@ -1,8 +0,0 @@
-let mongoose = require('mongoose');
-
-let calculationProgressSchema = new mongoose.Schema({
-    isAlgorithmFinished : [Boolean] 
-});
-
-const CalculationProgress = mongoose.model("CalculationProgress", calculationProgressSchema)
-module.exports.CalculationProgress = CalculationProgress;
