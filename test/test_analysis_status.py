93dd3108e8c27e4fa3e5bf7ca4b8fe544730061f
Madghostek <madghostek@gmail.com>
2023-12-14 13:43:40 +0100

    PZSP2-12 python api ingertaion tests

---
+++ test/test_analysis_status.py
@@ -0,0 +1,17 @@
+import requests
+
+url = "http://localhost:1234/analysis"
+
+def test_status():
+	json_data = requests.post(url+"/new", files={'file': ('nazwa','zawartosc')}).json()
+	assert 'token' in json_data
+
+	res = requests.get(url+"/status/"+str(json_data['token']))
+
+	assert res.status_code in (200,202) # either ready, or in progress
+
+def test_status_no_token():
+	assert requests.get(url+"/status/").status_code == 500
+
+# def test_status_invalid_token():
+# 	assert requests.get(url+"/status/nonsense").status_code == 404
