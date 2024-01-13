f91e219177cc77d0d2585c2fa92b9a9fcb905bf3
Madghostek <madghostek@gmail.com>
2024-01-14 00:39:09 +0100

    queue and periodic requests working

--- nodejs_api/src/outer_connections.js
+++ nodejs_api/src/outer_connections.js
@@ -4,7 +4,7 @@ async function requestCalculation(fstream, serviceAddress, port=5000){
 	// fsteam - ReadStream of file to send
 	// serviceAddress - address of something that will perform calculations
 
-	let target = "http://"+serviceUrl+port+'/api/calculateStuff'
+	let target = "http://"+serviceAddress+port+'/api/calculateStuff'
 	return await axios.post(target, fstream).then((res)=>{
 		console.log("got response",res.data)
 		return res.data;
@@ -15,4 +15,21 @@ async function requestCalculation(fstream, serviceAddress, port=5000){
 	})
 }
 
-module.exports = {requestCalculation}
+async function askForStatus(serviceAddress, token, port=5000, onResponse){
+	// fsteam - ReadStream of file to send
+	// serviceAddress - address of something that will perform calculations
+	let target = "http://"+serviceAddress+":"+port+'/api/status?token='+token
+	console.log("making request to "+target)
+
+	return await axios.get(target).then((res)=>{
+		console.log("got response",res.data)
+		return res.data;
+	}).catch((error)=>{
+		console.log("error")
+		if (error.code=="ECONNREFUSED")
+			console.log("Unable to connect to "+serviceAddress);
+		return {"error":error.code}
+	})
+}
+
+module.exports = {requestCalculation, askForStatus}
