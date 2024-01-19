d8b47791b977134d2a5e1cd1e1d6be55a98091a9
Madghostek <madghostek@gmail.com>
2024-01-19 13:40:47 +0100

    split pangolin and spip into 2

---
+++ algorithms/03_pangolin/containers/00_container1/src/main.py
@@ -0,0 +1,39 @@
+#!/bin/env python
+
+import json
+from flask import Flask, jsonify, request
+import os
+import subprocess
+
+print("out.vcf exists?:",os.path.isfile('out.vcf'))
+
+API_PREFIX = "/api/"
+app = Flask(__name__)
+@app.route(API_PREFIX + "status", methods=["GET"])
+def status():
+    # check if output file has been created by program, then return it
+    command = "pangolin "
+    result = subprocess.run(command, shell=True, text=True)
+    print(result.stdout)
+    try:
+        f = open('out.vcf')
+    except FileNotFoundError:
+        return {"done":False}
+
+    with f:
+        lines = f.readlines()
+    return {"done":True,"result":lines}
+
+
+@app.route(API_PREFIX + "calculateStuff", methods=["POST"])
+def calculateStuff():
+    record = json.loads(request.data)
+    # can do some quick file checks here, or just pass to alg
+    if all("#CHROM" not in r for r in record):
+        return {"error":"missing header"},400
+
+    return {"status":"ok"}
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
